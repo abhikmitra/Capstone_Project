0c60ef3dff33018e36462fa56f955d60581f6691
Abhik Mitra <abhikprince@gmail.com>
2015-08-16 16:01:22 +0530

    Added Fresco

--- MaterializeYourApp-master/MaterializeYourApp-master/app/src/main/java/com/mitra/abhik/humansoftheworld/RecyclerViewAdapter.java
+++ MaterializeYourApp-master/MaterializeYourApp-master/app/src/main/java/com/mitra/abhik/humansoftheworld/RecyclerViewAdapter.java
@@ -16,15 +16,29 @@
 
 package com.mitra.abhik.humansoftheworld;
 
+import android.graphics.drawable.Animatable;
+import android.net.Uri;
 import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.squareup.picasso.Picasso;
+import com.facebook.common.logging.FLog;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.controller.BaseControllerListener;
+import com.facebook.drawee.controller.ControllerListener;
+import com.facebook.drawee.drawable.ProgressBarDrawable;
+import com.facebook.drawee.generic.GenericDraweeHierarchy;
+import com.facebook.drawee.generic.GenericDraweeHierarchyBuilder;
+import com.facebook.drawee.interfaces.DraweeController;
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.facebook.imagepipeline.image.ImageInfo;
+import com.facebook.imagepipeline.image.QualityInfo;
+import com.facebook.imagepipeline.request.ImageRequest;
+import com.facebook.imagepipeline.request.ImageRequestBuilder;
 
 import java.util.List;
 
@@ -48,10 +62,68 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     @Override public void onBindViewHolder(ViewHolder holder, int position) {
+        final ViewHolder Fholdar = holder;
+        if(!holder.getRecycled()){
+            GenericDraweeHierarchyBuilder builder =
+                    new GenericDraweeHierarchyBuilder(holder.image.getContext().getResources());
+            GenericDraweeHierarchy hierarchy = builder
+                    .setFadeDuration(300)
+                    .setProgressBarImage(new ProgressBarDrawable())
+                    .build();
+            holder.image.setHierarchy(hierarchy);
+            holder.setRecycled(true);
+        }
         ViewModel item = items.get(position);
         holder.text.setText(item.getText());
         holder.image.setImageBitmap(null);
-        Picasso.with(holder.image.getContext()).load(item.getImage()).into(holder.image);
+        ControllerListener controllerListener = new BaseControllerListener<ImageInfo>() {
+            @Override
+            public void onFinalImageSet(
+                    String id,
+                    @Nullable ImageInfo imageInfo,
+                    @Nullable
+                    Animatable anim) {
+                if (imageInfo == null) {
+                    return;
+                }
+                Fholdar.image.setAspectRatio((float) imageInfo.getWidth() / imageInfo.getHeight());
+                QualityInfo qualityInfo = imageInfo.getQualityInfo();
+                FLog.d("Final image received! " +
+                                "Size %d x %d",
+                        "Quality level %d, good enough: %s, full quality: %s",
+                        imageInfo.getWidth(),
+                        imageInfo.getHeight(),
+                        qualityInfo.getQuality(),
+                        qualityInfo.isOfGoodEnoughQuality(),
+                        qualityInfo.isOfFullQuality());
+            }
+
+            @Override
+            public void onIntermediateImageSet(String id, @Nullable ImageInfo imageInfo) {
+//                    FLog.d("Intermediate image received");
+                Fholdar.image.setAspectRatio((float) imageInfo.getWidth() / imageInfo.getHeight());
+            }
+
+            @Override
+            public void onFailure(String id, Throwable throwable) {
+                FLog.e(getClass(), throwable, "Error loading %s", id);
+            }
+        };
+        String urlString = item.getImage();
+        if(urlString!=null){
+            Uri uri = Uri.parse(urlString);
+            ImageRequest request = ImageRequestBuilder.newBuilderWithSource(uri)
+                    .setProgressiveRenderingEnabled(true)
+                    .build();
+            DraweeController controller = Fresco.newDraweeControllerBuilder()
+                    .setImageRequest(request)
+                    .setControllerListener(controllerListener)
+                    .setOldController(holder.image.getController())
+                    .build();
+            holder.image.setController(controller);
+
+        }
+        //Picasso.with(holder.image.getContext()).load(item.getImage()).into(holder.image);
         holder.itemView.setTag(item);
     }
 
@@ -71,13 +143,22 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     protected static class ViewHolder extends RecyclerView.ViewHolder {
-        public ImageView image;
+        public SimpleDraweeView image;
         public TextView text;
-
+        Boolean recycled;
         public ViewHolder(View itemView) {
             super(itemView);
-            image = (ImageView) itemView.findViewById(R.id.image);
+            image = (SimpleDraweeView) itemView.findViewById(R.id.image);
             text = (TextView) itemView.findViewById(R.id.text);
+            recycled = false;
+        }
+
+        public Boolean getRecycled() {
+            return recycled;
+        }
+
+        public void setRecycled(Boolean recycled) {
+            this.recycled = recycled;
         }
     }
 
