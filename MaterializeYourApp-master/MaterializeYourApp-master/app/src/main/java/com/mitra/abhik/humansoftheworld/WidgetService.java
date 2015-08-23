eb3425df51f6001f3d30aa3c27beac8f64131a3b
Abhik Mitra <abhikprince@gmail.com>
2015-08-24 00:36:19 +0530

    Making the widget

--- MaterializeYourApp-master/MaterializeYourApp-master/app/src/main/java/com/mitra/abhik/humansoftheworld/WidgetService.java
+++ MaterializeYourApp-master/MaterializeYourApp-master/app/src/main/java/com/mitra/abhik/humansoftheworld/WidgetService.java
@@ -58,10 +58,10 @@ public class WidgetService extends RemoteViewsService {
             if(c.moveToLast()){
                 String picture = c.getString(Constants.COL_POST_PICTURE);
                 String message = c.getString(Constants.COL_POST_MESSAGE);
-                rv.setTextViewText(R.id.description, message);
+                //rv.setTextViewText(R.id.description, message);
                 try {
                     Bitmap b = Picasso.with(getApplicationContext()).load(picture).get();
-                    rv.setImageViewBitmap(R.id.picture, b);
+                    //rv.setImageViewBitmap(R.id.picture, b);
                 }
                 catch (Exception e){
 
