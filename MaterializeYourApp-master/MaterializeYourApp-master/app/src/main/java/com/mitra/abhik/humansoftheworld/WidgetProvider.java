982edfd84b6e537882f0fa5f7aafc7dfae7330c1
Abhik Mitra <abhikprince@gmail.com>
2015-08-24 01:39:34 +0530

    Making the widget

--- MaterializeYourApp-master/MaterializeYourApp-master/app/src/main/java/com/mitra/abhik/humansoftheworld/WidgetProvider.java
+++ MaterializeYourApp-master/MaterializeYourApp-master/app/src/main/java/com/mitra/abhik/humansoftheworld/WidgetProvider.java
@@ -6,8 +6,12 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
+import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.widget.RemoteViews;
+
+import com.mitra.abhik.humansoftheworld.data.PostsContract;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by abmitra on 8/22/2015.
@@ -19,7 +23,8 @@ public class WidgetProvider extends AppWidgetProvider {
         AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
         ComponentName thisAppWidget = new ComponentName(context.getPackageName(), WidgetProvider.class.getName());
         int[] appWidgetIds = appWidgetManager.getAppWidgetIds(thisAppWidget);
-        appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds,  R.id.listView);
+        //appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds,  R.id.listView);
+        onUpdate(context,appWidgetManager,appWidgetIds);
     }
 
     @Override
@@ -48,16 +53,30 @@ public class WidgetProvider extends AppWidgetProvider {
                                 int appWidgetId) {
 
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
-        views.setTextViewText(R.id.widgetText, "Widget");
+       // views.setTextViewText(R.id.widgetText, "Widget");
         Intent intent = new Intent(context, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
         views.setOnClickPendingIntent(R.id.container, pendingIntent);
+        Cursor c  = context.getContentResolver().query(PostsContract.PostEntry.buildUriForPosts(),
+                Constants.POST_COLUMNS, null, null, Constants.POST_COLUMNS[Constants.COL_CREATED_DATE] + " DESC");
+        if(c.moveToLast()){
+            String picture = c.getString(Constants.COL_POST_PICTURE);
+            String message = c.getString(Constants.COL_POST_TITLE);
+            views.setTextViewText(R.id.description, message);
+            try {
+                Bitmap b = Picasso.with(context).load(picture).get();
+                views.setImageViewBitmap(R.id.picture, b);
+            }
+            catch (Exception e){
 
+            }
 
-        Intent rvsintent = new Intent(context, WidgetService.class);
-        rvsintent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-        rvsintent.setData(Uri.parse(rvsintent.toUri(Intent.URI_INTENT_SCHEME)));
-        views.setRemoteAdapter(R.id.listView, rvsintent);
+        }
+
+//        Intent rvsintent = new Intent(context, WidgetService.class);
+//        rvsintent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+//        rvsintent.setData(Uri.parse(rvsintent.toUri(Intent.URI_INTENT_SCHEME)));
+//        views.setRemoteAdapter(R.id.listView, rvsintent);
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
